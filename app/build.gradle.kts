4d2c5fb22f3350c21357d604fb87dc3c235346da
Nur Hamid <nurhamidqq@gmail.com>
2023-10-23 14:48:08 +0700

    fix gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,23 +64,15 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material:material:1.5.4")
-//    implementation("androidx.core:core-ktx:1.6.0")
-//    implementation ("com.google.android.material:material:1.4.0")
-//    implementation("androidx.compose.ui:ui:1.0.2")
-//    implementation("androidx.compose.material:material:1.0.2")
-//    implementation("androidx.compose.ui:ui-tooling-preview:1.0.2")
-//    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-//    implementation("androidx.activity:activity-compose:1.3.1")
     testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-//    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-//    debugImplementation("androidx.compose.ui:ui-tooling:1.0.2")
-//    debugImplementation("androidx.compose.ui:ui-test-manifest:1.0.2")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     //compose dependencies
-//    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0-beta01")
     implementation ("androidx.navigation:navigation-compose:2.5.3")
     implementation ("androidx.compose.material:material-icons-extended:1.0.2")
     implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
@@ -99,7 +91,4 @@ dependencies {
 
     // Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:2.3.0")
-
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
 }
